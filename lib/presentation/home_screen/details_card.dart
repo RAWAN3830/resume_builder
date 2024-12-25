16c45c78e03ccfb4273bbfba9f935a2763227672
RAVAN <138647473+RAWAN3830@users.noreply.github.com>
2024-12-25 21:22:56 +0530

    feat:some_changes

--- lib/presentation/home_screen/details_card.dart
+++ lib/presentation/home_screen/details_card.dart
@@ -9,10 +9,13 @@ import 'package:resume/presentation/Declaration.dart';
 import 'package:resume/presentation/Project.dart';
 import 'package:resume/presentation/home_screen/skill_sets_screen.dart/skill_sets_screen.dart';
 import 'package:resume/presentation/pdf_generator.dart';
-import 'package:resume/presentation/skill_sets_screen.dart';
+import 'package:resume/presentation/carrier_objectibve.dart';
 
+import '../../core/constant/strings.dart';
+import '../Achievements.dart';
 import '../Education.dart';
 import '../Experiences.dart';
+import '../References.dart';
 import '../contact_info.dart';
 
 class DetailsCard extends StatelessWidget {
@@ -42,7 +45,7 @@ class DetailsCard extends StatelessWidget {
               context.push(context, target: const ProjectInfo());
               // context.push(context, target: const ExperienceInfo());
             case 5:
-              context.push(context, target:  const ResumeCreator());
+              context.push(context, target:  ResumeCreator());
               // context.push(context, target: const Achievement());
               // context.push(context, target: const TechSkills());
             case 6:
